6cd54bd55076e14fea79561e0fdc16a305517e02
Romi Tshiorny <romitshiorny@gmail.com>
2024-08-26 16:33:41 -0700

    fix encoding

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
-import csv
 from functools import reduce
 
-string = "CVOTNMPALIRE"
+string = "ANLRGDHWETIB"
 sides = list(map(lambda index: string[index:index + int(len(string)/4)], range(0, len(string), int(len(string)/4))))
 print(sides)
 
@@ -15,7 +14,7 @@ print(box)
 
 
 def isLegal(valid_sides, word_to_check):
-    
+
     # Three 'illegal cases'
     # Case 1: Word is 3 letters or shorter
     if len(word_to_check) <= 3:
@@ -43,29 +42,26 @@ def isLegal(valid_sides, word_to_check):
 
 def getLegalWords(valid_sides):
     valid_chars = ''.join(valid_sides)
-    directory = ".\\Words\\EOWL CSV Format"
+    directory = ".\\Words\\EOWL LF Delimited Format"
     word_files = os.listdir(directory)
     legal_files = []
     legal_words = {}
     for file in word_files:
 
         # All words are pre-sorted into files based on the letter they start with
-        # Follows the name pattern 'X Words.csv' where X is the starting letter
+        # Follows the name pattern 'X Words.txt' where X is the starting letter
         if valid_chars.__contains__(file[0]):
             legal_files.append(file)
 
     for file in legal_files:
-        with open(f"{directory}\\{file}", mode='r') as text:
-            try:
-                words = csv.reader(text)
-                for word in words:
-                    word_upper = word[0].upper()
-                    if isLegal(valid_sides, word_upper):
-                        legal_words.update(
-                            {word_upper[0]: [word_upper] + (legal_words.get(word_upper[0], []))}
-                        )
-            finally:
-                continue
+        with open(f"{directory}\\{file}", mode='r', encoding="utf-8") as text:
+            words = text.read().splitlines()
+            for word in words:
+                word_upper = word.upper()
+                if isLegal(valid_sides, word_upper):
+                    legal_words.update(
+                        {word_upper[0]: [word_upper] + (legal_words.get(word_upper[0], []))}
+                    )
 
     return legal_words
 
